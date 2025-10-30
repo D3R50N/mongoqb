574b862b2569c425e6c7356cc749afd1e8e68899
Max Dev <maxandersonmdr@gmail.com>
2025-10-30 16:34:55 +0100

    support cjs

--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export function query(modelOrName: string | import("mongoose").Model<any, any, any, any, any, any>): QueryBuilder;
-export { QueryBuilder };
-import QueryBuilder from "./core/query-builder.js";
